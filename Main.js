c99894ebfa5c3cb6cb97ae85fa7e72a758259c22
Max Lopes <maxlopes365@gmail.com>
2022-11-23 09:44:17 +0000

    Initial commit

---
+++ Main.js
@@ -0,0 +1,36 @@
+const btnLoadMore = document.getElementById("btnLoadMore");
+const pokemonList = document.getElementById("pokemonList");
+let limit = 10;
+let offset = 0;
+
+
+function loadPokemonItens(offset, limit) {
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newHtml = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            <img src="${pokemon.photo}" alt="${pokemon.name}"/>
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types
+                      .map((type) => `<li class="type ${type}">${type}</li>`)
+                      .join("")}
+                </ol>
+            </div>
+        </li>     
+      ` ).join("")
+
+    pokemonList.innerHTML += newHtml
+  });
+}
+
+
+loadPokemonItens(offset, limit)
+
+btnLoadMore.addEventListener('click', () => {
+    console.log("passou")
+    offset += limit
+    loadPokemonItens(offset, limit)
+  });
+  
